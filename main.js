aeaf1240ac582ec2165131cc7a2ea3feb3c8daa3
ibsby <16ismith98@gmail.com>
2020-07-04 03:28:18 -0400

    Added type writer effect to landing page

--- main.js
+++ main.js
@@ -1,38 +1,111 @@
-function typeWriter() {
-	//Get the Html element that I want to put an effect on
-	var text = document.querySelector(".type");
-	//Get the words that I want cycle through for with the type effect
-	var descriptors = JSON.parse(text.getAttribute("data-descriptors"));
-	console.log(typeof(['test', 'two']))
-	console.log(descriptors[0]);
-	
-	for(i = 0; i < descriptors.length; i++) {
-		//descriptor
-		console.log(`type ${i}`);
+class TypeWriter {
+    constructor(txtElement, text, descriptors) {
+        this.txtElement = txtElement;
+        this.text = text;
+        this.descriptors = descriptors;
+		this.descriptorText = '';
+        this.descriptorIndex = 0;
+        this.isDeleting = false;
+		this.doneCycling = false;
+		this.descriptorElement;
+		this.count = 0;
+    }
+
+	type() {
+
+		var txtElementLength = this.txtElement.innerText.length;
 		
-		typeDescriptor(descriptors[i], i);
+		// If we have displayed all of the preliminary text (the text before the descriptive words)
+		if(this.text.length == this.count) {
+			
+			//initialize the element that will hold the text for each descriptor
+			this.descriptorElement = this.txtElement.appendChild(document.createElement('span'));
+			//this.descriptorElement.setAttribute('class', 'txt');
+			
+			//Type each descriptive word
+			this.typeDescriptor();
+			
+		} else {
+			//Add a letter
+			this.count++;
+			this.txtElement.innerText = this.text.substring(0, this.count);
+			// Wait 100ms before typing the next letter
+			setTimeout(() => {this.type()}, 100);
+		}
 		
 	}
-	
-	function typeDescriptor(/*descriptor, index*/) {
-		for(n = 0; n < descriptors[i].length; n++){
-			var descriptor = descriptors[i].substring(0,n);
-			console.log(descriptor);
-		}
-		
-		setTimeout(1000 *2);
-		// If it is not the last descriptor delete the word before typing the next one
-		if(i != descriptors.length - 1){
-			//after two seconds delete the descriptor
-			//setTimeout(typeDescriptor, 1000*2)
-			//setTimeout(deleteDescriptor, 1000*2)
-		}
-			
-		function deleteDescriptor() {
-			console.log("delete");
-		}
 
+	typeDescriptor() {
+		
+		var current = this.descriptorIndex;
+		var textLength = this.descriptorText.length;
+		var descriptorLength = this.descriptors[current].length;
+		var typeSpeed = 100;	//The default time between keystrokes
+		
+		
+		if(!this.doneCycling) {
+			if(this.isDeleting) {
+				this.descriptorText = this.descriptors[current].substring(0,this.descriptorText.length - 1);
+
+				typeSpeed = 75;	//typeSpeed is shorter when deleting a word
+
+				//If you've deleted the word then move onto the next descriptor
+				
+				if(textLength == 0) {
+					this.descriptorIndex++;
+					this.isDeleting = false;
+					
+					typeSpeed = 400;
+				}
+				
+			} else {
+				this.descriptorText = this.descriptors[current].substring(0,this.descriptorText.length + 1);
+				
+				//If the word is spelled, then start deleting it
+				if(textLength == descriptorLength) {
+					this.isDeleting = true;
+					typeSpeed = 800;	//typeSpeed is longer when the word is finished
+					
+					//If this was the last descriptor, then you can stop cycling threw words
+					if(this.descriptorIndex == this.descriptors.length - 1) {
+						this.doneCycling = true;
+						//Remove the cursor after 1 seconds
+						setTimeout(() => {this.txtElement.removeAttribute("class")}, 1000);
+					}
+					
+				}
+			}
+			
+
+			
+			this.descriptorElement.innerText = this.descriptorText;
+			
+			
+			//Call the type message after 3 seconds
+			setTimeout(() => this.typeDescriptor(), typeSpeed);
+			}
+	
 	}
 }
 
-window.onload = typeWriter;
+
+window.onload = init();
+
+function init() {
+	// The parent HTML element that will have a typing animation run on it
+	var element = document.querySelector(".type");
+	
+	// The HTML element that will have its innerText field modified
+	var txtElement = element.appendChild(document.createElement('span'));
+	txtElement.setAttribute('class', 'txt');
+	
+	// The text that will be displayed in txtElement
+	var text = 'I am a curious and dependable computer science graduate who designs great ';
+	
+	//The descriptive words at the end of txtElement that will be cycled through
+	var descriptors = JSON.parse(element.getAttribute("data-descriptors"));
+	
+	//Initialize typewriter
+	new TypeWriter(txtElement, text, descriptors).type();
+}
+
