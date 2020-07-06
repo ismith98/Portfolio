24131fb909f880df840644276135e04cedd240f2
ibsby <16ismith98@gmail.com>
2020-07-05 21:44:52 -0400

    Edited css grid and added comments

--- main.js
+++ main.js
@@ -1,34 +1,47 @@
+window.onload = init();
+
+function init() {
+	// The parent HTML element that will have a typing animation run on it
+	var txtElement = document.querySelector(".type");
+	
+	// The text that will be displayed in txtElement
+	var text = 'I am a curious and dependable computer science graduate who designs great ';
+	
+	//The descriptive words at the end of txtElement that will be cycled through
+	var descriptors = JSON.parse(txtElement.getAttribute("data-descriptors"));
+	
+	//Initialize typewriter
+	new TypeWriter(txtElement, text, descriptors).type();
+}
+
 class TypeWriter {
-    constructor(txtElement, text, descriptors) {
+    constructor(txtElement, text, descriptors = {}) {
         this.txtElement = txtElement;
         this.text = text;
         this.descriptors = descriptors;
-		this.descriptorText = '';
-        this.descriptorIndex = 0;
-        this.isDeleting = false;
-		this.doneCycling = false;
-		this.descriptorElement;
-		this.count = 0;
+		this.descriptorElement; 	// The span element for descriptors
+		this.descriptorText = ''; 	// The text displayed in the descriptors span element
+        this.descriptorIndex = 0; 	// The current descriptor
+        this.isDeleting = false; 	// Start the backspace animation on the descriptors
+		this.doneCycling = false;	// Are we done cycling through descriptors?
+		this.textIndex = 0; 	//The index of the curent letter of the main text to be typed
     }
 
 	type() {
-
-		var txtElementLength = this.txtElement.innerText.length;
 		
 		// If we have displayed all of the preliminary text (the text before the descriptive words)
-		if(this.text.length == this.count) {
+		if(this.text.length == this.textIndex) {
 			
-			//initialize the element that will hold the text for each descriptor
+			// Initialize the element that will hold the text for each descriptor
 			this.descriptorElement = this.txtElement.appendChild(document.createElement('span'));
-			//this.descriptorElement.setAttribute('class', 'txt');
 			
-			//Type each descriptive word
+			// Type each descriptive word
 			this.typeDescriptor();
 			
 		} else {
-			//Add a letter
-			this.count++;
-			this.txtElement.innerText = this.text.substring(0, this.count);
+			// Add a letter
+			this.textIndex++;
+			this.txtElement.innerText = this.text.substring(0, this.textIndex);
 			// Wait 100ms before typing the next letter
 			setTimeout(() => {this.type()}, 100);
 		}
@@ -36,48 +49,59 @@ class TypeWriter {
 	}
 
 	typeDescriptor() {
+		// The index of the current descriptor
+		var current = this.descriptorIndex; 	
 		
-		var current = this.descriptorIndex;
-		var textLength = this.descriptorText.length;
+		// The length of the current descriptor
 		var descriptorLength = this.descriptors[current].length;
-		var typeSpeed = 100;	//The default time between keystrokes
 		
+		// The amount of letters of the current descriptor that is already spelled out
+		var typedTextLength = this.descriptorText.length; 	
 		
+		//The default time between keystrokes
+		var typeSpeed = 100;	
+		
+		// If there are still more descriptors to type out
 		if(!this.doneCycling) {
+			// If the text needs the backspace effect
 			if(this.isDeleting) {
-				this.descriptorText = this.descriptors[current].substring(0,this.descriptorText.length - 1);
+				// Remove a letter from the end of the displayed text
+				this.descriptorText = this.descriptors[current].substring(0, typedTextLength - 1);
 
-				typeSpeed = 75;	//typeSpeed is shorter when deleting a word
+				//typeSpeed is shorter when deleting a word
+				typeSpeed = 75;
 
 				//If you've deleted the word then move onto the next descriptor
-				
-				if(textLength == 0) {
+				if(typedTextLength == 0) {
 					this.descriptorIndex++;
 					this.isDeleting = false;
 					
-					typeSpeed = 400;
+					// Pause before typing the next word
+					typeSpeed = 500;
 				}
 				
 			} else {
-				this.descriptorText = this.descriptors[current].substring(0,this.descriptorText.length + 1);
+				// Add a letter to the end of the displayed text
+				this.descriptorText = this.descriptors[current].substring(0, typedTextLength + 1);
 				
-				//If the word is spelled, then start deleting it
-				if(textLength == descriptorLength) {
+				//If the word is completely spelled, then start deleting it
+				if(typedTextLength == descriptorLength) {
 					this.isDeleting = true;
-					typeSpeed = 800;	//typeSpeed is longer when the word is finished
 					
-					//If this was the last descriptor, then you can stop cycling threw words
+					//typeSpeed is longer when the word is finished
+					typeSpeed = 800;	
+					
+					//If this was the last descriptor, then you can stop cycling through words
 					if(this.descriptorIndex == this.descriptors.length - 1) {
 						this.doneCycling = true;
 						//Remove the cursor after 1 seconds
-						setTimeout(() => {this.txtElement.removeAttribute("class")}, 1000);
+						setTimeout(() => {this.txtElement.removeAttribute("class"); }, 1000);
 					}
 					
 				}
 			}
 			
-
-			
+			// Display the new text
 			this.descriptorElement.innerText = this.descriptorText;
 			
 			
@@ -87,25 +111,3 @@ class TypeWriter {
 	
 	}
 }
-
-
-window.onload = init();
-
-function init() {
-	// The parent HTML element that will have a typing animation run on it
-	var element = document.querySelector(".type");
-	
-	// The HTML element that will have its innerText field modified
-	var txtElement = element.appendChild(document.createElement('span'));
-	txtElement.setAttribute('class', 'txt');
-	
-	// The text that will be displayed in txtElement
-	var text = 'I am a curious and dependable computer science graduate who designs great ';
-	
-	//The descriptive words at the end of txtElement that will be cycled through
-	var descriptors = JSON.parse(element.getAttribute("data-descriptors"));
-	
-	//Initialize typewriter
-	new TypeWriter(txtElement, text, descriptors).type();
-}
-
