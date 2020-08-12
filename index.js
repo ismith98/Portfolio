27ea7f9bc05893c9d8d14be490e6b468b8f85df8
Isaac Smith <16ismith98@gmail.com>
2020-08-12 18:55:09 -0400

    Added procfile and made port file work on heroku

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var path = require('path');
 const fetch = require("node-fetch");
 
+var PORT = process.env.PORT || 3000;
 
 //app.use(express.static(path.join(__dirname + '/images')))
 
@@ -17,7 +18,7 @@ app
   .get('/projects/MeditationTimer', (req,res) => res.sendFile(path.join(__dirname, 'public/ProjectsPage/MedTimer/index.html')))
   .get('/projects/Converter', (req,res) => res.sendFile(path.join(__dirname, 'public/ProjectsPage/Converter/index.html')))
   .get('/projects/Converter/Exchange', async(req, res) => createProxyServer(req, res))
-  .listen(3000, () => console.log('App is listening on port 3000'));
+  .listen(PORT, () => console.log(`App is listening on port ${PORT}`));
 
   // Server for to get the exchange rates for the currency converter
   async function createProxyServer(req, res) {
