759aa52eca2467ce9b943fc4bc96c20f99d5838b
Isaac Smith <16ismith98@gmail.com>
2020-08-12 19:39:29 -0400

    index changes

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ var PORT = process.env.PORT || 3000;
 //app.use(express.static(path.join(__dirname + '/images')))
 
 app
-  .use(express.static('./public'))
+  .use(express.static(path.join(__dirname, './public')))
   /*
   .use(express.static(path.join(__dirname, '/public/ProjectsPage' )))
   .use(express.static(path.join(__dirname, '/public/ProjectsPage/MedTimer' )))
   .use(express.static(path.join(__dirname, '/public/ProjectsPage/MedTimer/audio' )))*/
   .get('/', (req,res) => res.sendFile(path.join(__dirname, './public/HomePage/index.html')))
-  .get('/projects',(req,res) => {res.sendFile(path.join(__dirname, '/public/ProjectsPage/projects.html'))})
-  .get('/projects/MeditationTimer', (req,res) => res.sendFile(path.join(__dirname, 'public/ProjectsPage/MedTimer/index.html')))
-  .get('/projects/Converter', (req,res) => res.sendFile(path.join(__dirname, 'public/ProjectsPage/Converter/index.html')))
+  .get('/projects',(req,res) => {res.sendFile(path.join(__dirname, './public/ProjectsPage/projects.html'))})
+  .get('/projects/MeditationTimer', (req,res) => res.sendFile(path.join(__dirname, './public/ProjectsPage/MedTimer/index.html')))
+  .get('/projects/Converter', (req,res) => res.sendFile(path.join(__dirname, './public/ProjectsPage/Converter/index.html')))
   .get('/projects/Converter/Exchange', async(req, res) => createProxyServer(req, res))
   .listen(PORT, () => console.log(`App is listening on port ${PORT}`));
 
