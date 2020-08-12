fe325ccddba8767f7641351143a5d45a30407fd6
Isaac Smith <16ismith98@gmail.com>
2020-08-12 19:01:45 -0400

    removed path join from express for homepage

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app
   .use(express.static(path.join(__dirname, '/public/ProjectsPage' )))
   .use(express.static(path.join(__dirname, '/public/ProjectsPage/MedTimer' )))
   .use(express.static(path.join(__dirname, '/public/ProjectsPage/MedTimer/audio' )))*/
-  .get('/', (req,res) => res.sendFile(path.join(__dirname, '/public/HomePage/index.html')))
+  //.get('/', (req,res) => res.sendFile(path.join(__dirname, '/public/HomePage/index.html')))
+  .get('/', (req,res) => res.sendFile( '/public/HomePage/index.html'))
   .get('/projects',(req,res) => {res.sendFile(path.join(__dirname, '/public/ProjectsPage/projects.html'))})
   .get('/projects/MeditationTimer', (req,res) => res.sendFile(path.join(__dirname, 'public/ProjectsPage/MedTimer/index.html')))
   .get('/projects/Converter', (req,res) => res.sendFile(path.join(__dirname, 'public/ProjectsPage/Converter/index.html')))
